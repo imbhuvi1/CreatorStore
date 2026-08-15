ba6f6bd9af01cda28a8835192d57503dde6d465f
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-08-15 14:49:26 +0530

    [Bhuvnesh]: Initial Commit

---
+++ CreatorStore/src/main/java/com/bhuvnesh/creatorstore/CreatorStoreApplication.java
@@ -0,0 +1,19 @@
+package com.bhuvnesh.creatorstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStoreApplication {
+	public static void main(String[] args) {
+		//Dotenv configirations
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach((entry) -> System.setProperty(
+				entry.getKey(),entry.getValue()
+		));
+
+		SpringApplication.run(CreatorStoreApplication.class, args);
+	}
+}
---
+++ CreatorStore/src/test/java/com/bhuvnesh/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuvnesh.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
