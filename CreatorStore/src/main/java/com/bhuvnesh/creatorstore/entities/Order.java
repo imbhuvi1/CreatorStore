e5b0b8b9b835393558437c0181fa5672a7bdecb4
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-08-15 20:05:18 +0530

    [Bhuvnesh]: Refactor. Added @JsonBackReference and cascade property

--- CreatorStore/src/main/java/com/bhuvnesh/creatorstore/entities/Order.java
+++ CreatorStore/src/main/java/com/bhuvnesh/creatorstore/entities/Order.java
@@ -1,5 +1,6 @@
 package com.bhuvnesh.creatorstore.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -31,7 +32,8 @@ public class Order {
     @Column(name = "total_price",nullable = false)
     private BigDecimal totalPrice;
 
-    @OneToMany(mappedBy = "order")
+    @JsonBackReference
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems;
 
     @Column(name = "created_at")
